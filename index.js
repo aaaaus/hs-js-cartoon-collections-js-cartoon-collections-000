2fa912884e1b380a23bb429e3c7d5ab6c7ef8bef
Austin Luft <ausluft@gmail.com>
2018-08-23 03:57:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function dwarfRollCall(dwarves) {
   for (i = 0; i < dwarves.length; i++) {
 	dwarfList.push([i+1] + `. ` + dwarves[i] + ` `)
 }
-var string = `${dwarfList[1]}
-return numList
+var string = `${dwarfList[1]}${dwarfList[2]}${dwarfList[3]}`
+return string
 }
 
 
