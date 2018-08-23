c1203ec28f455a84160b0af0a4e45ab659f267c2
Austin Luft <ausluft@gmail.com>
2018-08-23 03:55:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ let i = 0;
 function dwarfRollCall(dwarves) {
   while (i < dwarves.length) {
 	list.push([i+1] + `. ` + dwarves[i])
-	i++
 }
 return numList
 }
 
 
 function summonCaptainPlanet(planeteerCalls){
+  for (i)
 }
 
 function longPlaneteerCalls(words) {
